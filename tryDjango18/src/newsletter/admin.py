1e62669b868499d2bb219e6be1416cbe9dae7510
AbrarKhan <abrar.nitk@gmail.com>
2018-03-31 11:01:26 +0530

    changes

--- tryDjango18/src/newsletter/admin.py
+++ tryDjango18/src/newsletter/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import SignUp
+
+class AdminSignUp(admin.ModelAdmin):
+	list_display = ["__unicode__", "full_name", "timestamp", "updates"]
+	class Meta:
+		model = SignUp
+
+
+admin.site.register(SignUp, AdminSignUp)
