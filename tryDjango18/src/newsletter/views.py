851070f3e5a86dab58e72719710458cfffab9d45
AbrarKhan <abrar.nitk@gmail.com>
2018-04-29 16:46:34 +0530

    changes for form mail sending

--- tryDjango18/src/newsletter/views.py
+++ tryDjango18/src/newsletter/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from .forms import SignUpForm
-
+from .forms import SignUpForm, ContactForm
+from django.conf import settings
+from django.core.mail import send_mail
 
 # Create your views here.
 
@@ -31,3 +32,41 @@ def home(request):
         }
 
     return render(request, 'home.html', context)
+
+
+# this is not the model form so we can't save it directly
+def contact(request):
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        # for key in form.cleaned_data:
+        #     print key, form.cleaned_data.get(key)
+        form_email = form.cleaned_data.get("email")
+        form_message = form.cleaned_data.get("message")
+        form_full_name = form.cleaned_data.get("full_name")
+        subject = 'Site contact form'
+        from_email = settings.EMAIL_HOST_USER
+        to_mail = [from_email]
+        html_message = """"
+            <h1>Hellow</h1>
+        """
+        contact_message = "%s: %s via %s" %(
+            form_full_name,
+            form_message,
+            form_email
+        )
+        send_mail(
+            subject,
+            contact_message,
+            from_email, to_mail,
+            html_message=html_message,
+            fail_silently=True
+        )
+
+
+    # print(request.POST)
+
+    context = {
+        "form": form
+    }
+
+    return render(request, "forms.html", context)
